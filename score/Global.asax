fd7f54febe9d8bf0f8752d733f7943a26d1294f3
BurroughsD <92319508+BurroughsD@users.noreply.github.com>
2022-12-12 16:13:02 -0500

    Add project files.

---
+++ score/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="score.MvcApplication" Language="C#" %>
---
+++ score/Views/SalesBoardAllMTDsController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using score.Models;
+using System.Globalization;
+
+namespace score.Views
+{
+    public class SalesBoardAllMTDsController : Controller
+    {
+        private SalesCommissionEntities db = new SalesCommissionEntities();
+
+        // GET: SalesBoardAllMTDs
+        public ActionResult Index(string location = "")
+        {
+            DateTimeFormatInfo dtfi = CultureInfo.GetCultureInfo("en-US").DateTimeFormat;
+            int days = DateTime.DaysInMonth(DateTime.Now.Year, DateTime.Now.Month);
+
+
+            ViewBag.ReportDate = dtfi.GetMonthName(DateTime.Now.Month) + " " + (DateTime.Now.Year.ToString());
+            ViewBag.DIM = days;
+
+            if (location != "")
+            {
+                ViewBag.Location = location;
+                return View(db.SalesBoardAllMTDs.Where(a => a.LOCATION == location).OrderByDescending(a => a.MTD).ToList());
+            }
+            else
+            {
+                ViewBag.Location = " ALL Fitzgerald";
+                return View(db.SalesBoardAllMTDs.OrderByDescending(a => a.MTD).ToList());
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
