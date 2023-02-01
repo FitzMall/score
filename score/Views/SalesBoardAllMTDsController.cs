8911d8983ccc4235b1196d71fa2151e90f508f78
BurroughsD <92319508+BurroughsD@users.noreply.github.com>
2023-02-01 14:26:46 -0500

    remove transfers  a.sl_VehicleCategory != 'T'

--- score/Views/SalesBoardAllMTDsController.cs
+++ score/Views/SalesBoardAllMTDsController.cs
@@ -103,7 +103,7 @@ namespace score.Views
             DateTime ReportDate = dReportDate;
             ViewBag.MonthDisplay = ReportDate.ToString("MMMM");
             ViewBag.YrShow = ReportDate.ToString("yyyy");
-            var SBoard = MTD.Where(a => a.dept_code == Team).OrderBy(a => a.SalesID + a.SalesRank ).ToList();
+            var SBoard = MTD.Where(a => a.dept_code == Team).OrderBy(a => a.SalesRank + a.SalesID).ToList();
 
             //            return View(MTD.Where(a => a.dept_code == Team).OrderBy(a => a.SalesID).ToList());
             return View(SBoard);
